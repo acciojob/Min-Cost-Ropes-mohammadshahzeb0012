de9f2d310f1f74616bf5d077d91a0cfbfb28e7b8
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-20 01:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
 
+function mincost(arr){ 
+  let sum=0;
+while(arr.length>1){
+	arr.sort((a,b)=>a-b);
+	let a = arr.shift();
+	let b = arr.shift();
+	sum+=a+b;
+	arr.push(a+b);
+}
+	return sum;
+}
 module.exports=mincost;
